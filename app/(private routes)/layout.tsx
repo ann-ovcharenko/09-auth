728db99350b398c265cf937bf9e5f7056c3aae88
Anna Ovcharenko <anyutka1426@gmail.com>
2025-12-29 16:35:57 +0200

    change layout.tsx

--- app/(private routes)/layout.tsx
+++ app/(private routes)/layout.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 interface PrivateLayoutProps {
   children: React.ReactNode;
-  modal: React.ReactNode;
+  modal?: React.ReactNode;
 }
 
 export default function PrivateLayout({ children, modal }: PrivateLayoutProps) {
   return (
-    <>
+    <div className="private-layout-wrapper">
       {children}
       {modal}
-    </>
+    </div>
   );
 }
